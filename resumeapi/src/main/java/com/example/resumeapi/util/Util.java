8cb0442ca613aa65d650526ae1dd85cfdd01901b
satya <satyach8189@gmail.com>
2020-08-16 10:31:54 +0530

    new chages

--- resumeapi/src/main/java/com/example/resumeapi/util/Util.java
+++ resumeapi/src/main/java/com/example/resumeapi/util/Util.java
@@ -2,8 +2,9 @@ package com.example.resumeapi.util;
 
 public class Util {
 	public static void main(String a[]) {
-		System.out.println("welcome to util method");
-		System.out.println("welcome to util method123");
+		System.out.println("welcome1 to util method");
+		System.out.println("welcome2 to util method123");
+		System.out.println("welcome3 to util method123");
 	}
 
 }
